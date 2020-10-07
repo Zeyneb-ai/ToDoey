bccfff1c22ccffa99e23b4de1c1691aa009fb702
ZK <Znahidekaya@gmail.com>
2020-10-06 19:20:52 -0700

    Create new tasks, check completed tasks, bugs fixed in todo
    Using new plist and encoder/decoder

--- ToDoey/Controllers/TodoListViewController.swift
+++ ToDoey/Controllers/TodoListViewController.swift
@@ -9,17 +9,22 @@ import UIKit
 
 class TodoListViewController : UITableViewController {
     
-    var itemArray = [String]()
-    
-    let defaults = UserDefaults.standard
-    
+    var itemArray = [Item]()
+    let dataFilePath  = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
-            itemArray = items
-        }
+        
+        
+        
+        let newItem1 = Item()
+        newItem1.title = "Learn to Code"
+        itemArray.append(newItem1)
+        
+        loadItems()
+        
         tableView.separatorStyle = .none
         
     }
@@ -30,8 +35,12 @@ class TodoListViewController : UITableViewController {
         return itemArray.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = itemArray[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -41,13 +50,10 @@ class TodoListViewController : UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row]
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -64,10 +70,13 @@ class TodoListViewController : UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what happens when add item is clicked
             if textField.text!.trimmingCharacters(in: .whitespaces) != "" {
-                self.itemArray.append(textField.text!)
-                self.defaults.set(self.itemArray, forKey: "TodoListArray")
+               
+                let newItem = Item()
+                newItem.title = textField.text!
+                self.itemArray.append(newItem)
                 
-                self.tableView.reloadData()
+                self.saveItems()
+                
                 
             }
             
@@ -83,5 +92,39 @@ class TodoListViewController : UITableViewController {
         
         present(alert, animated: true, completion: nil )
     }
+    
+    func saveItems()  {
+        
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath! )
+        } catch {
+            print("encoding error: \(error)")
+        }
+        self.tableView.reloadData()
+        
+        
+    }
+    
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+            itemArray = try decoder.decode([Item].self , from: data)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by NebbyKookie on 10/6/20.
+//
+
+import Foundation
+
+class Item: Codable {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
